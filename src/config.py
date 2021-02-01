119eea8055f43bbe398c727aa2fc4d7439ac4ccd
git@github.com/michTalebzadeh <mich.talebzadeh@gmail.com>
2021-02-01 16:17:10 +0000

    url definition in python as yml cannot do concatenation building strings!

--- src/config.py
+++ src/config.py
@@ -3,6 +3,7 @@ with open("../conf/config.yml", 'r') as file:
   config: dict = yaml.load(file.read(), Loader=yaml.FullLoader)
   #print(config.keys())
   #print(config['hiveVariables'].items())
+  hive_url = "jdbc:hive2://" + config['hiveVariables']['hiveHost'] + ':' + config['hiveVariables']['hivePort'] + '/default'
   oracle_url = "jdbc:oracle:thin:@" + config['OracleVariables']['oracleHost'] + ":" + config['OracleVariables']['oraclePort'] + ":" + config['OracleVariables']['oracleDB']
 
 
